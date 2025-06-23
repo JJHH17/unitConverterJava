ea4e1e7c1df2d97ca05225cb0f75078289bca8a7
James Hatfield <james.hatfield17@icloud.com>
2025-06-23 16:30:20 +0100

    Added class and methods to convert and read celsius and fahrenheit

---
+++ src/unit/convert/Temperature.java
@@ -0,0 +1,28 @@
+package unit.convert;
+
+public class Temperature {
+
+    private int fahrenheit;
+    private int celsius;
+
+    public Temperature(int fahrenheit, int celsius) {
+        this.fahrenheit = fahrenheit;
+        this.celsius = celsius;
+    }
+
+    public void getFahrenheit(int celsius) {
+        this.fahrenheit = (celsius * 9/5) + 32;
+    }
+
+    public String getFahrenheit() {
+        return this.fahrenheit + "°F";
+    }
+
+    public void getCelsius(int fahrenheit) {
+        this.celsius = (fahrenheit - 32) * 5/9;
+    }
+
+    public String getCelsius() {
+        return this.celsius + "°C";
+    }
+}
