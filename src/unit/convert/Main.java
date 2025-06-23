90fe80c7bfc76028d31cfc84be3d206a151fb96d
James Hatfield <james.hatfield17@icloud.com>
2025-06-23 16:55:02 +0100

    Added program loop that allows user to convert celsius to fahrenheit

--- src/unit/convert/Main.java
+++ src/unit/convert/Main.java
@@ -1,7 +1,27 @@
 package unit.convert;
+import java.util.Scanner;
 
 public class Main {
     public static void main(String[] args) {
-        System.out.println("Hello world");
+        Scanner scanner = new Scanner(System.in);
+
+        while (true) {
+            System.out.println("Convert Fahrenheit (type 'to fahrenheit') or Celsius? (type 'to celsius') (type 'exit' to quit)");
+            String userInput = scanner.nextLine();
+
+            if (userInput.equalsIgnoreCase("exit")) {
+                break;
+            }
+
+            if (userInput.equalsIgnoreCase("to celsius")) {
+                System.out.println("Enter a temperature in celsius:");
+                int celsiusInput = Integer.valueOf(scanner.nextLine());
+
+                Temperature celsiusToFahrenheit = new Temperature(0, celsiusInput);
+                celsiusToFahrenheit.calcFahrenheit(celsiusInput);
+
+                System.out.println(celsiusToFahrenheit.getFahrenheit());
+            }
+        }
     }
 }
