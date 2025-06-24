b2741f3a8375d6290bbb09013543cb54a03c4a48
James Hatfield <james.hatfield17@icloud.com>
2025-06-24 14:35:32 +0100

    Added method calculations for unit conversions

--- src/unit/convert/Weight.java
+++ src/unit/convert/Weight.java
@@ -7,50 +7,50 @@ public class Weight {
     private double ounces;
 
     public void gramsToKilograms(double grams) {
-
+        this.kilograms = grams / 1_000;
     }
 
     public void gramsToPounds(double grams) {
-
+        this.pounds = grams / 453.6;
     }
 
     public void gramsToOunces(double grams) {
-
+        this.ounces = grams / 28.35;
     }
 
     public void kilogramsToGrams(double kilograms) {
-
+        this.grams = kilograms * 1000;
     }
 
     public void kilogramsToPounds(double Kilograms) {
-
+        this.pounds = kilograms * 2.205;
     }
 
     public void kilogramsToOunces(double kilograms) {
-
+        this.ounces = kilograms * 35.274;
     }
 
     public void poundsToGrams(double pounds) {
-
+        this.grams = pounds * 453.6;
     }
 
     public void poundsToKilograms(double pounds) {
-
+        this.kilograms = pounds / 2.205;
     }
 
     public void poundsToOunces(double pounds) {
-
+        this.ounces = pounds * 16;
     }
 
     public void ouncesToGrams(double ounces) {
-
+        this.grams = ounces * 28.35;
     }
 
     public void ouncesToKilograms(double ounces) {
-
+        this.kilograms = ounces / 35.274;
     }
 
     public void ouncesToPounds(double ounces) {
-
+        this.pounds = ounces / 16;
     }
 }
