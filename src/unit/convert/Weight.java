2638410a19feeb614476105947d5b463ccfe4805
James Hatfield <james.hatfield17@icloud.com>
2025-06-24 14:16:06 +0100

    Created weight class, created methods that will convert measurements

---
+++ src/unit/convert/Weight.java
@@ -0,0 +1,56 @@
+package unit.convert;
+
+public class Weight {
+    private double grams;
+    private double kilograms;
+    private double pounds;
+    private double ounces;
+
+    public void gramsToKilograms(double grams) {
+
+    }
+
+    public void gramsToPounds(double grams) {
+
+    }
+
+    public void gramsToOunces(double grams) {
+
+    }
+
+    public void kilogramsToGrams(double kilograms) {
+
+    }
+
+    public void kilogramsToPounds(double Kilograms) {
+
+    }
+
+    public void kilogramsToOunces(double kilograms) {
+
+    }
+
+    public void poundsToGrams(double pounds) {
+
+    }
+
+    public void poundsToKilograms(double pounds) {
+
+    }
+
+    public void poundsToOunces(double pounds) {
+
+    }
+
+    public void ouncesToGrams(double ounces) {
+
+    }
+
+    public void ouncesToKilograms(double ounces) {
+
+    }
+
+    public void ouncesToPounds(double ounces) {
+
+    }
+}
